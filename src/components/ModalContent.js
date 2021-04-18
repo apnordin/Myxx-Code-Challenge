b5e257874198d479d86d13bd2ecc67e38e51a852
Alex Nordin <apnordin@gmail.com>
2021-04-18 19:53:36 -0400

    Run build, ready to deploy

--- src/components/ModalContent.js
+++ src/components/ModalContent.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import React from "react";
 
 export default function ModalContent ( {rocketImage, rocketName, rocketStatus, rocketDesc, engineStages, engineType, engineThrust}) {
 
