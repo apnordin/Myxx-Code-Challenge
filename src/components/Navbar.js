667e999412c0031bf5be6df7fe47212088789aa0
Alex Nordin <apnordin@gmail.com>
2021-04-28 11:50:31 -0400

    Conditional rendering for vehicle divs vs slider, slider not functional yet

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -3,20 +3,35 @@ import spacex from "../assets/spacex.png";
 
 export default function Navbar() {
 
+
+    const DynamicNavbar = () => {
+        if (window.innerWidth > 762) {
+            return (
+                <nav className="navbar pt-5 justify-content-between">
+                    <img className="spacexlogo" alt="spacexlogo" src={spacex}></img>
+                    <span></span>
+                    <form className="form-inline">
+                        <button className="btn btn-round" type="button" onClick={openRepo}>
+                            Vehicles
+                        </button>
+                    </form>
+                </nav>
+            )
+        } else {
+            return (
+                <nav className="navbar">
+                    <img className="spacexlogo img-center" alt="spacexlogo" src={spacex}></img>
+                </nav>
+            )
+        }
+    }
+
     const openRepo = () => {
         const url = "https://github.com/apnordin/Myxx-Code-Challenge"
         window.open(url, "_blank");
     }
     
     return(
-        <nav className="navbar pt-5 justify-content-between">
-            <img className="spacexlogo" alt="spacexlogo" src={spacex}></img>
-            <span></span>
-            <form className="form-inline">
-                <button className="btn btn-round" type="button" onClick={openRepo}>
-                    Vehicles
-                </button>
-            </form>
-        </nav>
+        <DynamicNavbar />
     )
 }
--- src/pages/Main.js
+++ src/pages/Main.js
@@ -1,14 +1,30 @@
 import React from "react";
 import Navbar from "../components/Navbar";
-import PrimaryContent from "../components/PrimaryContent"
+import PrimaryContent from "../components/PrimaryContent";
+import Carousel from "../components/Carousel";
 
 export default function Main () {
+
+
+    const MainContent = () => {
+        if (window.innerWidth > 762) {
+            return (
+                <PrimaryContent />
+            )
+        } else {
+            return (
+                <Carousel />
+            )
+        } 
+        
+    }
+
     return (
         <div className="background-web allpage">
             <Navbar />
             <div className="container">
                 <div className="row">
-                    <PrimaryContent />
+                    <MainContent />
                 </div>
             </div>
         </div>
